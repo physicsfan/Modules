2b146209495f6c8d6f6f836827eb9de3398c0e9a
Andrew Senchuk <asenchuk@cube.cs.ubc.ca>
2020-01-02 16:49:07 -0800

    moved find_index to genint_module

--- genint_module.f90
+++ genint_module.f90
@@ -94,7 +94,54 @@ contains
       end do
       
     end subroutine genint
-          
+
+    !> function to find the location of an integral in the list
+    INTEGER FUNCTION find_index ( k, label )
+
+    !> k     -- integral type, k<0 is I integral
+    !> index -  index of an integral in canonical order
+    !           (ia <= ib; if k <0)
+    !           (ia <= ic; ib <= id; ia <= ib  if k>=0)
+    Integer, intent(in)  :: k, label
+
+    !>  il -- lower bound
+    !>  iu -- upper bound
+    !>  im -- mid-point
+    Integer  :: il, im, iu
+    
+    if (k < 0) then
+       il =1; iu = Int_end(1)
+    else
+       il = Int_end(k+1)+1 ; iu = Int_end(k+2)
+    end if
+
+    do
+      if (iu-il >  1) then
+         im = (iu + il)/2
+         if ( label > int_label(im)) then
+           il = im
+         else if ( label < int_label(im)) then
+           iu = im
+         else if (label == int_label(im)) then
+           index = im    
+           exit
+         end if
+       else if (iu-il == 1) then
+         if ( label == int_label(iu)) then
+           index = iu
+           exit
+         else if ( label ==  int_label(il)) then
+           index = il 
+           exit
+         else
+            index = -1
+            Print *, index, ' not found'
+            exit
+         end if
+      end if
+    end do
+  END FUNCTION find_index
+    
     subroutine write_labels
      
     Integer :: i, lab, k, ia, ib, ic, id, istart, n
